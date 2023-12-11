49b440515311c3d97b1ef561a0d1e8e7ed6e2033
BytePrashant <prashanttomar765@gmail.com>
2023-12-11 22:48:47 +0530

    changed font

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,7 +1,7 @@
 export function Appbar() {
   return (
     <div>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 font-custom1">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="#"
