45a7b40f0a195779f233f3a56a1837ee0678153a
BytePrashant <prashanttomar765@gmail.com>
2023-12-11 22:48:13 +0530

    changed font

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,10 @@
 export function About() {
   return (
     <div>
-      <section id="about" className="relative flex flex-col py-36">
+      <section id="about" className="relative flex flex-col py-36 font-custom1">
         <div id="container" className="w-[1070px] h-[355px] mx-[249px] px-10">
           <div id="content" className="w-[990px] h-[355px] flex items-center">
-            <div
-              id="img-side"
-              className="relative w-[495px] h-[354px] "
-            >
+            <div id="img-side" className="relative w-[495px] h-[354px] ">
               <img
                 src="/working_emoji.png"
                 alt="emoji"
@@ -19,10 +16,7 @@ export function About() {
                 className="main-img object-cover rounded-lg w-[410px] h-[350px]"
               />
               <span>
-                <img
-                  src="/text.svg"
-                  alt="text"
-                />
+                <img src="/text.svg" alt="text" />
               </span>
             </div>
             <div className="text-side  flex flex-col w-[495px] h-[354px]">
@@ -32,16 +26,16 @@ export function About() {
               <h4 className="h-[70px] text-2xl mb-4 font-bold">
                 A dedicated Front-end Developer <br /> based in India 📍
               </h4>
-              <p className="w-[480px] h-[238px] text-[18px] text-gray-700">
-                As a Junior Front-End Developer, I possess an impressive arsenal
-                of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I
-                excel in designing and maintaining responsive websites that
-                offer a smooth user experience. My expertise lies in crafting
-                dynamic, engaging interfaces through writing clean and optimized
-                code and utilizing cutting-edge development tools and
-                techniques. I am also a team player who thrives in collaborating
-                with cross-functional teams to produce outstanding web
-                applications.
+              <p className="w-[480px] h-[238px] text-[18px] text-gray-800/60">
+                As a Full-Stack Web Developer, I possess an impressive arsenal
+                of skills in HTML, CSS, JavaScript, React, Node.js, Tailwind,
+                and MUI. I excel in designing and maintaining responsive
+                websites that offer a smooth user experience. My expertise lies
+                in crafting dynamic, engaging interfaces through writing clean
+                and optimized code and utilizing cutting-edge development tools
+                and techniques. I am also a team player who thrives in
+                collaborating with cross-functional teams to produce outstanding
+                web applications.
               </p>
             </div>
           </div>
