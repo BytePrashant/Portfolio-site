c70e066bb29ac779bc6223286b726cb4c876d13b
BytePrashant <prashanttomar765@gmail.com>
2023-12-12 01:12:50 +0530

    left with just animation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,41 +1,49 @@
 export function About() {
   return (
     <div>
-      <section id="about" className="relative flex flex-col py-36 font-custom1">
-        <div id="container" className="w-[1070px] h-[355px] mx-[249px] px-10">
-          <div id="content" className="w-[990px] h-[355px] flex items-center">
-            <div id="img-side" className="relative w-[495px] h-[354px] ">
+      <section id="about" className="relative flex flex-col py-28 font-custom1">
+        <div id="container" className="w-[1070px] h-[450px] mx-[249px] px-10">
+          <div id="content" className="w-[990px] h-[450px] flex items-center">
+            <div id="img-side" className="relative w-[495px] h-auto">
+              <img
+                src="/computer.webp"
+                alt="me"
+                className="main-img object-cover rounded-2xl w-[410px] h-[350px] relative z-0"
+              />
+              <span className="absolute bottom-0 right-0 w-auto h-auto bg-white rounded-full origin-top-left rotate-12">
+                <img
+                  src="https://www.stefantopalovic.com/static/media/text2.3d5aa6ba2d0632bb4e0572631c3f9dc2.svg"
+                  alt="text"
+                  className="h-[190px] w-[190px] animate-rotate-slow"
+                />
+              </span>
               <img
                 src="/working_emoji.png"
                 alt="emoji"
-                className="work-emoji absolute bottom-35 right-98 w-24 z-10"
+                className="work-emoji absolute bottom-14 right-[90px] w-14  overflow-hidden"
               />
-              <img
-                src="/computer.webp"
-                alt="mee"
-                className="main-img object-cover rounded-lg w-[410px] h-[350px]"
-              />
-              <span>
-                <img src="/text.svg" alt="text" />
-              </span>
             </div>
-            <div className="text-side  flex flex-col w-[495px] h-[354px]">
-              <h3 className="font-[sans-serif] font-bold uppercase text-blue-500 text-base mb-3">
+            <div className="text-side  flex flex-col w-[495px] h-auto">
+              <h3 className="font-[sans-serif] font-bold uppercase text-blue-500 text-base mb-1">
                 About me
               </h3>
-              <h4 className="h-[70px] text-2xl mb-4 font-bold">
-                A dedicated Front-end Developer <br /> based in India 📍
+              <h4 className="h-auto text-2xl mb-2 font-semibold">
+                A not so dedicated Full Stack Web Developer
               </h4>
-              <p className="w-[480px] h-[238px] text-[18px] text-gray-800/60">
-                As a Full-Stack Web Developer, I possess an impressive arsenal
-                of skills in HTML, CSS, JavaScript, React, Node.js, Tailwind,
-                and MUI. I excel in designing and maintaining responsive
-                websites that offer a smooth user experience. My expertise lies
-                in crafting dynamic, engaging interfaces through writing clean
-                and optimized code and utilizing cutting-edge development tools
-                and techniques. I am also a team player who thrives in
-                collaborating with cross-functional teams to produce outstanding
-                web applications.
+              <p className="w-[480px] h-auto text-[16px] text-gray-800/60 text-justify">
+                A Full Stack Web Developer who learned the art of coding in the
+                same time it takes most people to decide what to watch on
+                Netflix. With the magical powers of a quick learner, I've
+                mastered the art of building beautiful, user-friendly websites.
+                They say laughter is the best medicine, but I believe a
+                well-written code snippet is even better. As a team player, I
+                assure you that my newfound skills will make me an irreplaceable
+                asset to any web development team. Because, you know, who needs
+                years of experience when you've got six months of absolute
+                brilliance under your belt? So, if you're looking for a
+                developer with a sense of humor as sharp as their coding skills,
+                look no further. I promise my websites are not only
+                user-friendly but also certified chuckle-embedded.
               </p>
             </div>
           </div>
